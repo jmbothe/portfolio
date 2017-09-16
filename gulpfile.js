289fe18b0139cf67099d32102dbe6d6a2a8e0a05
Jeff Bothe <jeffrymbothe@gmail.com>
2017-09-16 11:42:11 -0500

    hero 02

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,59 @@
 const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
 
 gulp.task('css', () => {
   const postcss = require('gulp-postcss');
 
-  return gulp.src('styles/styles.css')
+  return gulp.src('app/styles/**/*.css')
+    .pipe(plumber())
     .pipe(postcss([require('autoprefixer')]))
-    .pipe(gulp.dest('dist/styles/'));
+    .pipe(gulp.dest('dist/styles/'))
+    .pipe(reload({stream: true}));
 });
+
+gulp.task('scripts', () => {
+  const uglify = require('gulp-uglify');
+  const rename = require('gulp-rename');
+  gulp.src('app/scripts/**/*.js')
+    .pipe(plumber())
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('html', () => {
+  gulp.src('app/**/*.html')
+  .pipe(gulp.dest('dist'))
+  .pipe(reload({stream: true}));
+})
+
+gulp.task('assets:cleanfolder', (cb) => {
+  const del = require('del');
+  return del([
+    'dist/assets/**',
+  ], cb)
+})
+
+gulp.task('assets:copy', ['assets:cleanfolder'], () => {
+  return gulp.src(['app/assets/*', '!app/assets/psd/'])
+    .pipe(gulp.dest('dist/assets'))
+})
+
+gulp.task('browser-sync', () => {
+  browserSync ({
+    server: {
+      baseDir: './dist'
+    }
+  })
+})
+
+gulp.task('watch', () => {
+  gulp.watch('app/scripts/**/*.js', ['scripts']);
+  gulp.watch('app/styles/**/*css', ['css']);
+  gulp.watch('app/**/*.html', ['html']);
+})
+
+gulp.task('default', ['scripts', 'css', 'html', 'assets:copy', 'browser-sync', 'watch']);
