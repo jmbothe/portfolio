3cad5cd4511d76381b3e0c70ce96fc78b03d89ef
Jeff Bothe <jeffrymbothe@gmail.com>
2017-09-18 21:29:29 -0500

    animating projects 2

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,21 +1,20 @@
 'use strict';
 
-$('.projects-grid').on('click', '.project-hidden',  function(e) {
-    const visibleProject = $('.project-shell:not(.project-hidden)')
-    const headingAndLinks = visibleProject.children('h3, .project-links')
-    const textAndBorder = visibleProject.children('p, .project-inner')
-    const shell = $(e.target).closest('.project-hidden')
+$('.projects-grid').on('click', '.project-hide',  function(e) {
+    const current = $('.project-shell:not(.project-hide)')
+    const target = $(e.target).closest('.project-hide')
 
-    headingAndLinks.addClass('collapse-cross')
+    setTimeout(function() {
+        current.addClass('project-collapse')
+    }, 0)
 
+    setTimeout(function() {
+        current.addClass('project-hide')
+        target.removeClass('project-hide')
+    }, 550)
 
-    textAndBorder
-        .animate({
-            opacity: 0
-        }, 300, function () {
-            textAndBorder.css('opacity', 1)
-            headingAndLinks.removeClass('collapse-cross')
-            visibleProject.addClass('project-hidden')
-            shell.removeClass('project-hidden')
-        })
+    setTimeout(function() {
+        target.removeClass('project-collapse')
+    }, 1100)
+
 })
